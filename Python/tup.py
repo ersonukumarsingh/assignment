d0a92a5ec97a71f762c7d3942c8a4afcfbe802dd
Sonu Kumar Singh <er.sonusingh93@gmail.com>
2024-05-18 14:39:22 +0530

    .

--- Python/tup.py
+++
@@ -1,8 +0,0 @@
-import sys
-import timeit
-print(timeit.timeit(stmt="[0,1,2,3,4,5]",number=100000))
-print(timeit.timeit(stmt="(0,1,2,3,4,5)",number=100000))
-my_list = [0,1,2,"hello","True"],
-my_tuple = (0,1,2,"hello","True")
-print(sys.getsizeof(my_list),"bytes")
-print(sys.getsizeof(my_tuple),"bytes")
