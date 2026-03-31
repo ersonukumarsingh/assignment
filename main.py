167a345160048f0b385bd9a7ac7c115fde0079be
Sonu Kumar Singh <er.sonusingh93@gmail.com>
2026-03-31 12:05:59 +0530

    main.py

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ print("Hello, World!   This is a simple Python program.")
 
 name = input("What is your name? ")
 print(f"Hello, {name}! Welcome to the world of Python programming!")
+
+# This program demonstrates basic input and output in Python.
+
+#addition of two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is: {sum}")
